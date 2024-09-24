8027834b5228bb0f16058083d74386c9336bb7cb
Traven <TRAVEN.BLEVINS9061@STU.MTEC.EDU>
2024-09-24 08:53:48 -0600

    to do list

--- style.js
+++ style.js
@@ -1,13 +1,40 @@
-const button = document.querySelector('.button')
-const input = document.getElementById('input')
-const list = document.querySelector('.list-title')
+const button = document.querySelector('.top-button');
+const input = document.getElementById('input');
+const list = document.querySelector('.list-title');
 
-button.addEventListener('click', function add (){
-    const inputValue = input.value
+// Declare newButton and taskInput outside the event listener
+
+
+button.addEventListener('click', function add() {
+    const inputValue = input.value;
     if (inputValue !== '') {
-        const newItem = document.createElement('li')
-        newItem.textContent = inputValue
-        list.appendChild(newItem)
-        input.value = ''
+        const newItem = document.createElement('li');
+
+        const text = document.createTextNode(inputValue);
+        newItem.append(text);
+
+        // Now these variables are assigned globally (in scope of this file)
+        taskInput = document.createElement('input');
+        taskInput.classList.add('taskInput')
+        newItem.append(taskInput);
+
+        const newButton = document.createElement('button');
+        newButton.textContent = 'Add a task';
+        newItem.append(newButton);
+
+        list.appendChild(newItem);
+        input.value = '';
+        newButton.addEventListener('click', function () {
+            if (taskInput !== '') {
+                const task = document.createElement('h3')
+                task.textContent = taskInput.value
+                newItem.appendChild(task)
+                taskInput.value = ''
+            }
+        })
     }
-})
+    
+});
+
+
+
